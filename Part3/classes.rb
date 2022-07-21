767a5e0fe961e59929581d36deafa71c9e6faa5b
Denis280303 <kozlovskyidenus@gmail.com>
2022-07-21 22:12:17 +0300

    Update classes.rb

--- Part3/classes.rb
+++ Part3/classes.rb
@@ -1,26 +1,29 @@
 #Класс Station (Станция):
 #Имеет название, которое указывается при ее создании
-#Может принимать поезда (по одному за раз)
 class Station
-  attr_reader :name, :trains
+  attr_reader :trains, :name
   def initialize(name)
     @name = name
     @trains = []
   end
 
-  #Может показывать список всех поездов на станции, находящиеся в текущий момент
-  def exist(train)
+  #Приймаємо потяг
+  def set_train(train)
     @trains << train unless trains.include?(train)
   end
 
-  #Может отправлять поезда (по одному за раз, при этом, поезд 
-  #удаляется из списка поездов, находящихся на станции).
-  def away(train)
+  #Показуємо всі потяги
+  def list_all_trains
+    @trains
+  end
+
+  #Відправляє потяг з поточної станції
+  def send_train(train)
     @trains.delete(train) if trains.include?(train)
   end
 
-  #Может показывать список поездов на станции по типу: кол-во грузовых, пассажирских
-  def show_type(train)
+  #Показуємо потяги залежно від типу
+  def list_trains_by_type(type)
     @trains.select { │train│ train.type == type}
   end
 end
@@ -32,19 +35,24 @@ end
 #а промежуточные могут добавляться между ними.
 #Может выводить список всех станций по-порядку от начальной до конечной
 class Route
-  attr_reader :way
-  def initialize(start, finish)
-    @way = [start, finish]
+  attr_reader :stations
+  def initialize(first, last)
+    @stations = [first, last]
   end
 
-  #Может добавлять промежуточную станцию в список
-  def add(station)
-    @way.insert(-2, station) unless @way.include?(station)
+  #Додаємо проміжну станцію
+  def add_another_station(station)
+    @stations.insert(-2, station) unless @stations.include? station
   end
 
-  #Может удалять промежуточную станцию из списка
-  def del(station)
-    @way.delete(station) if @way.include?(station)
+  #Видалити проміжну станцію
+  def del_another_station(station)
+    @stations.delete(station) unless (@stations.first || @stations.last) != station
+  end
+
+  #Список всіх станцій
+  def list_stations
+    @stations.each { |item| puts item.name}
   end
 end
 
@@ -56,7 +64,7 @@ end
 #Может показывать количество вагонов
 #Показывать предыдущую станцию, текущую, следующую, на основе маршрута
 class Train
-  attr_reader :currently_speed, :wags, :curr_st
+  attr_reader :num, :type, :currently_speed, :wags, :current_station
   def initialize(num, type, wags)
     @num = num
     @type = type
@@ -64,69 +72,90 @@ class Train
     @currently_speed = 0
   end
 
-  #Может набирать скорость (вказуємо на скільки збільшити швидкість)
-  def speed_up(speed)
-    @currently_speed += speed
+  #Набираємо швидкість
+  def accelerate(value)
+    @currently_speed += value
   end
 
-  #Может тормозить (сбрасывать скорость до нуля)
-  def speed_down(speed)
-    @currently_speed == 0 ? @currently_speed = 0 : @currently_speed -= speed
+  #Поточна швидкість
+  def currently_speed
+    puts @currently_speed
   end
 
-  #Может прицеплять/отцеплять вагоны (по одному вагону за операцию, 
-  #метод просто увеличивает или уменьшает количество вагонов).
-  #користувач вказує "+" для додавання вагону і "-" для віднімання.
-  #Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
-  def wags_change(char)
-    if char == "+" && @currently_speed == 0
-      @wags += 1
-    end
-    if char == "-" && @currently_speed == 0
-      @wags -= 1
-    end
+  #Гальмуємо
+  def decelerate(value)
+    @currently_speed == 0 ? @currently_speed = 0 : @currently_speed -= value
+  end
+
+  #Метод для причеплення вагону
+  def attach_wagon
+    @wags += 1 if @currently_speed == 0
+  end
+
+  #Метод для відчеплення вагону
+  def detach_wagon
+    @wags -= 1 if @currently_speed == 0
+  end
+
+  #Кількість вагонів
+  def count_wagons
+    puts @wags
   end
 
   #Может принимать маршрут следования (объект класса Route)
-  def trip(route)
+  def get_route(route)
     @route = route
-    @curr_st = @route.way.first
-    @curr_st.exist(self)
+    @current_station = @route.stations[0]
+    @current_station.set_train(self)
+    @current_st_index = 0
   end
 
-  #Может перемещаться между станциями, указанными в маршруте.
-  def move
-    return unless @curr_st
-    curr_st_index = @route.way.find_index(@curr_st)
-    return unless curr_st_index + 1 < @route.way.count
-    @curr_st.delete(self)
-    curr_st_index = @route.way[curr_st_index + 1]
-    @last_st = @route.way[curr_st_index - 1]
-    @next_st = @route.way[curr_st_index + 1]
-    @curr_st.exist(self)
+  #Рух вперед
+  def move_forwards
+    return unless @route && next_station
+    current_station.delete(self)
+    @current_st_index += 1
+    @current_station.set_train(self)
+  end
+
+  #Рух назад
+  def move_backwards
+    return unless @route && previous_station
+    current_station.delete(self)
+    @current_st_index -= 1
+    @current_station.set_train(self)
   end
 
   #Попередня зупинка
-  def last
-    @last_st
+  def previous_station
+    return unless @route
+    puts @route.stations[@current_st_index - 1]
+  end
+
+  #Поточна станція
+  def current_station
+    return unless @route
+    @route.route[@current_st_index]
   end
 
   #Наступна зупинка
-  def next
-    @next_st
+  def next_station
+    return unless @route
+    puts @route.stations[@current_st_index + 1]
   end
 end
 
-start_st = Station.new("Vinnytsia")
-finish_st = Station.new("Khmelnytskiy")
-aditional_st1 = Station.new("Kozyatun")
-aditional_st2 = Station.new("Shepetivka")
-trip = Route.new(start_st, finish_st)
-trip.add(aditional_st1)
-trip.add(aditional_st1)
-trip.way
-my_train = Train.new("#293", "пасажирський", 7)
-my_train.move
-last_st = my_train.last
-next_st = my_train.next
+vinnytsia_station = Station.new("Vinnytsia")
+khmelytskiy_station = Station.new("Khmelnytskiy")
+kozyatun_station = Station.new("Kozyatun")
+shepetivka_station = Station.new("Shepetivka")
+route = Route.new(vinnytsia_station, khmelytskiy_station)
+route.add_another_station(kozyatun_station)
+route.add_another_station(shepetivka_station)
+route.list_stations
+train_293 = Train.new("#293", "пасажирський", 7)
+train_293.count_wagons
+train_293.move_forwards
+train_293.previous_station
+train_293.next_station
 
